233c88b45e209b2a1dcd635336ee4c54019490cd
Bernard-McGeever <Bernard.mcgeever@resonate.tech>
2021-03-16 19:22:22 +0000

    first commit

---
+++ src/uk/co/bernardmcgeever/Main.java
@@ -0,0 +1,17 @@
+package uk.co.bernardmcgeever;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    /*
+        Introduce myself and prints a tree to the screen
+        */
+        System.out.println("Hey there, I’m Bernard and I’m learning to code in Java!");
+        System.out.println("the goal of this short project is to print a tree to the console");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+    }
+}
